17b112a670acc4dfb01940719c195c82c3ebee86
ShockRate <blackhound05@yahoo.gr>
2024-02-28 06:14:07 +0200

    Begun RoomCard

--- src/components/RoomCard.tsx
+++ src/components/RoomCard.tsx
@@ -1,5 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Room } from '../types'
+import { Modal, ModalBody } from 'flowbite-react'
+
+
 
 type roomCardProps = {
     room:Room
@@ -8,9 +11,16 @@ type roomCardProps = {
 const RoomCard = ({room}:roomCardProps) => {
   return (
     <article className='my-4 pb-2 border-b border-gray-400'>
+      <div className="flex">
+        <div className="block h-auto w-1/4">
+          <img src={`assets/images/rooms/${room.attributes.photo_url}`} alt={`${room.attributes.photo_url}`} height="auto"/>
+        </div>
+      </div>
         {room.attributes.name}
     </article>
   )
 }
 
 export default RoomCard
+
+
