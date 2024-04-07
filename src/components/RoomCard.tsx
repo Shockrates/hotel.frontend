ed60d4520d27ee0766f584896a4ce4486dfcf6d2
ShockRate <blackhound05@yahoo.gr>
2024-04-07 23:43:12 +0300

    Additions to RoomCard

--- src/components/RoomCard.tsx
+++ src/components/RoomCard.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Room } from '../types'
+import { Booking, Room } from '../types'
 import { Modal, ModalBody } from 'flowbite-react'
 
 
@@ -7,27 +7,46 @@ import { Modal, ModalBody } from 'flowbite-react'
 type roomCardProps = {
    
   room:Room
+  booking?:Booking
 }
 
 const RoomCard = ({room}:roomCardProps) => {
   return (
     <article className='my-4 pb-2 border-b border-gray-400'>
-      <div className="flex">
-        <div className="block h-auto w-1/4">
-          <img src={`assets/images/rooms/${room.attributes.photo_url}`} alt={`${room.attributes.photo_url}`} height="auto"/>
-        </div>
-        <div className="relative w-4/5 ml-4 pl-6 box-border border-l-4 border-orange-500 text-left">
-          <h1 className='text-4xl font-bold'>{room.attributes.name}</h1>
-          <p className='text-neutral-500 font-semibold text-2xl'>{room.attributes.city}, {room.attributes.area}</p>
-          <p>{room.attributes.description_short}</p>
-          <div className="flex justify-end w-full text-right mt-6">
-            <p>Price:{room.attributes.price}</p>
-            <button className='flex bg-orange-500 items-center justify-center text-white text-base h-7 rounded-md px-1 py-5 my-1'>
-              <span>Go to room Page</span>
-            </button>
+      <div className="flex flex-col">
+        <div className="flex flex-col sm:flex-row">
+          <div className="block h-auto sm:w-1/4 w-full">
+            <img src={`assets/images/rooms/${room.attributes.photo_url}`} alt={`${room.attributes.photo_url}`} height="auto"/>
+          </div>
+          <div className="relative w-full sm:w-3/4 sm:ml-4 sm:pl-6 box-border sm:border-l-4 sm:border-orange-500 text-left">
+            <h1 className='text-4xl font-bold'>{room.attributes.name}</h1>
+            <p className='text-neutral-500 font-semibold text-2xl'>{room.attributes.city}, {room.attributes.area}</p>
+            <p>{room.attributes.description_short}</p>
+            <div className="flex justify-end w-full text-right mt-6">
+              
+              <button className='flex bg-orange-500 items-center justify-center text-white text-base h-7 rounded-md px-1 py-5 my-1'>
+                <span>Go to room Page</span>
+              </button>
+            </div>
           </div>
         </div>
+        <div className="flex mt-5">
+          <div className="w-full sm:w-1/4 bg-gray-600 text-lg font-semibold text-white py-1">
+            <p>Total Cost: {room.attributes.price}€</p>
+          </div>  
+          <div className="flex w-full  sm:w-3/4 sm:ml-4 sm:pl-6 bg-gray-100 text-lg text-gray-800 py-1 rounded-sm">
+            <div className="mx-auto px-2">
+              <p>Count of Guests: {room.attributes.count_of_guests}</p>
+            </div>
+            <div className="border-l-2 border-gray-400"></div>
+            <div className="mx-auto px-2">
+              <p>Room Type: {room.attributes.type}</p>
+            </div>
+            
+          </div>  
+        </div>
       </div>
+     
       
         
     </article>
